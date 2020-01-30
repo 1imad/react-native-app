caa14df007cd89be74f76028d00754333bb25626
Imad Ahmed <erimadahmad@gmail.com>
2020-01-30 16:47:57 +0530

    My very first react native app

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { Text, StyleSheet, View, Image } from 'react-native';
+import image from "../../assets/Imad.png";
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
+  const name = "Imad Ahmed"
   return (
-    <View>
-      <Text style={{ fontSize: 30 }}>HomeScreen</Text>
-      <Button title="Layout" onPress={() => navigation.navigate('Layout')} />
+    <View style={styles.mainView}>
+      <Text style={styles.text}>My first react native app.</Text>
+      <Image style={styles.image} source={image}></Image>
+      <Text style={styles.paragraph}>Hi, My name is <Text style={styles.bold}>{name}</Text></Text>
     </View>
   );
 };
 
-HomeScreen.navigationOptions = {
-  title: 'Home'
-};
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+    textTransform: "capitalize"
+  },
+  paragraph:{
+    fontSize: 15,
+    textAlign: "center"
+  },
+  image:{
+    height: 200,
+    width: 200,
+    alignSelf:"center",
+    marginBottom: 10,
+    borderRadius: 10
+  },
+  mainView: {
+    textAlign: "center"
+  },
+  bold:{
+    fontWeight: "bold"
+  }
+});
 
 export default HomeScreen;
